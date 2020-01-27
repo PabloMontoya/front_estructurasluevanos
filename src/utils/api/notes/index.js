9a6cfefec58814ebef70089be9ad578da7e883df
Pablo Montoya <pablomontoya392@gmail.com>
2020-01-26 20:30:35 -0800

    factorized api url

--- src/utils/api/notes/index.js
+++ src/utils/api/notes/index.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { apiUrl } from '../apiUrl';
 
 const getNotes = async () => {
     return await new Promise (async (resolve,reject) => {
-        const req = await axios.get('https://api-estructurasluevanos.herokuapp.com/notas')
+        const req = await axios.get(`${apiUrl}/notas`)
             .then(res => res.data)
             .catch((err) => reject(err));
         resolve(req);
